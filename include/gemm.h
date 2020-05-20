073b4994dd816891544d8a946317799e4f4d4cdc
zms1999 <48548452+zms1999@users.noreply.github.com>
2020-05-20 10:36:32 +0800

    use baseline as myGemm

--- include/gemm.h
+++ include/gemm.h
@@ -2,6 +2,22 @@
 #include "util.h"
 
 template<class T>
+__global__ void gemm(T *A, T *B, T *C, int m, int n, int k, T alpha, T beta) {
+    int col = blockIdx.x * blockDim.x + threadIdx.x;
+    int row = blockIdx.y * blockDim.y + threadIdx.y;
+    // printf("col %d row %d\n", col, row);
+    if( (col < n) && (row < m) )
+    {
+        T tmp = beta * C[row * n + col];
+        for(int i = 0; i < k; ++i)
+        {
+            tmp += alpha * A[row * k + i] * B[col + i * n];
+        }
+        C[row * n + col] = tmp;
+    }
+}
+
+template<class T>
 double myGEMM(T* A, T* B, T* C, T alpha, T beta)
 {
 	printf("perform your gemm here on m=%d n=%d k=%d\n", M, N, K);
@@ -19,6 +35,11 @@ double myGEMM(T* A, T* B, T* C, T alpha, T beta)
 	else
 	{
 		// your gemm
+        dim3 block(DIM_THREAD_BLOCK_X, DIM_THREAD_BLOCK_Y);
+        dim3 grid( (N + block.x - 1) / block.x, (M + block.y - 1) / block.y );
+        timestamp(t0);
+        gemm <<<grid, block>>>
+            (A, B, C, M, N, K, alpha, beta);
 
 		checkCudaErrors(cudaDeviceSynchronize());
 		return 0.f;	
